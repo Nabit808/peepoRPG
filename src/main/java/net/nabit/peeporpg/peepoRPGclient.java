3ee592e717d821f7761f7b320e6d2aeb0624bd48
BuildTools <unconfigured@null.spigotmc.org>
2024-02-11 14:35:57 +0200

    Initial commit

---
+++ src/main/java/net/nabit/peeporpg/peepoRPGclient.java
@@ -0,0 +1,10 @@
+package net.nabit.peeporpg;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class peepoRPGclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
